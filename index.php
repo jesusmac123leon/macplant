027bc9b1557590519bc703b1af1392ca9ae8d93a
jesus cruz <war.cancer@gmail.com>
2020-06-26 16:10:53 -0500

    error

---
+++ index.php
@@ -0,0 +1,45 @@
+	<html lang="en">
+	 
+	<head>
+	 <title>Registrar Usuario</title>
+	 <meta charset = "utf-8">
+	</head>
+	
+	<body>
+	 <header>
+	<center><h1>proyecto sistem like</h1></center> 
+	 </header> 
+	 
+	 <form action="registrar-usuario.php" method="post"> 
+	 <hr />
+
+	 <center><h3>facebook</h3>
+
+	 <img src="124010.png" width="200"
+     height="100">
+	 </center>
+
+	<center> <label for="nombre">correo:</label><br>
+	 <input type="text" name="username" maxlength="32" required>
+	 <br/><br/>
+	 
+	 <!--Password-->
+	 <label for="pass">Password:</label><br>
+	 <input type="password" name="password" maxlength="8"  required>
+	 
+	 <br/><br/>
+	 <input type="submit" name="submit" value="Registrarme">
+	 <input type="reset" name="clear" value="Borrar">
+	 </form></center>
+	 
+
+	<footer>
+	
+
+	</footer> 
+
+	 
+
+	 </body>
+
+	</html>
